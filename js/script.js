6ac4a7f16f316ff57eee5c7e3ae59256bbbb9e66
charlotte-jhu <124389290+charlotte-jhu@users.noreply.github.com>
2023-05-29 17:04:55 +0000

    finished website

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ const getWeather = async (URLAddress) => {
     jsonData.main.temp = Math.round(jsonData.main.temp - 273.15)
     jsonData.main.feels_like = Math.round(jsonData.main.feels_like - 273.15)
     document.getElementById("answer").innerHTML = "The weather is " + jsonData.weather[0].main + " with " + jsonData.weather[0].description + "<br /> <br />" + "The temperature right now is " + jsonData.main.temp + " feels like "+ jsonData.main.feels_like
-    document.getElementById("weather-icon").innerHTML = "<img src='https://openweathermap.org/weather-conditions#Icon-list'" + jsonData.weather[0].icon + ">"
+    document.getElementById("weather-icon").innerHTML = "<img src='http://openweathermap.org/img/wn/" + jsonData.weather[0].icon + "@2x.png'>"
   } catch (err) {
     console.log(err)
   }
